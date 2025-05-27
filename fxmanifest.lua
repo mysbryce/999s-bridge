0d8e13c228080c0fd2110b45982e4a8527cdbf77
999s <123440999+mysbryce@users.noreply.github.com>
2025-05-27 16:44:15 +0700

    fix version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games {'gta5'}
 
 name '999s Bridge'
 description 'An open source that make your code easier to understand'
-version '1.0.0'
+version '1.0.0-alpha-b1'
 author '999s <github:mysbryce>'
 
 --[[ for test
